cb6600bd247e0d12485022621726e69b6477c9a8
github username <diep.christopher@gmail.com>
2018-06-20 18:51:41 -0400

    started on question 3

--- question-3.rb
+++ question-3.rb
@@ -1,3 +1,8 @@
-def change_possibilities(amount,amount)
+def change_possibilities(amount,denominations)
+  #sort array and remove duplicates
+  ordered = denominations.sort.uniq
 
 end
+
+puts "4 expected"
+puts change_possibilities(4, [1,2,3])
