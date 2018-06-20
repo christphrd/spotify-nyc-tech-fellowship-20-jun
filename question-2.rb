72eae1608103605b2d7db047bfd99e87a912ead7
github username <diep.christopher@gmail.com>
2018-06-20 15:55:17 -0400

    only first case. closer with second case?

--- question-2.rb
+++ question-2.rb
@@ -1,25 +1,36 @@
 require 'pry'
 
 def decode_string(s)
-  #find innermost bracket
-  #repeat what's in bracket by num ("ab" * num)
-  #keep going until no more brackets
-  while s.include? "["
-    arr = s.split("[")
-    #["4", "ab]"]
-    binding.pry
-    num = arr.shift.to_i
-    #4, arr = ["ab]"]
-    new_str = arr.join("[")
-    #"ab]"
-    new_str_no_brack = new_str[0...-1]
-    #"ab"
-    s = new_str_no_brack * num
+  results_arr = []
+
+  arr = s.chars
+  datatype_arr = arr.map do |char|
+    if char.to_i != 0
+      char.to_i
+    else
+      char
+    end
+  end.reverse
+
+  datatype_arr.each_with_index do |element, index|
+    if element == "["
+      multiplied = datatype_arr[0..index] * datatype_arr[index + 1]
+      results_arr = multiplied
+    end
   end
-  s
+
+  # binding.pry
+
+  braced_results_str = results_arr.join("").reverse
+  decoded = braced_results_str.delete("[]")
 end
 
-# puts "abababab expected"
-# puts decode_string("4[ab]")
-puts "baaabaaa expected"
-puts decode_string("2[b3[a]]")
+puts "abababab expected"
+puts decode_string("4[ab]")
+# puts "baaabaaa expected"
+# puts decode_string("2[b3[a]]")
+
+#for loop equivalent
+# array.each_with_index do |element,index|
+#   element.do_stuff(index)
+# end
