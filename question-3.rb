ba0cbd147059770d0d61a50c1ab58259143ca028
github username <diep.christopher@gmail.com>
2018-06-21 00:08:13 -0400

    counting ways with multiples of one coin

--- question-3.rb
+++ question-3.rb
@@ -1,8 +1,18 @@
 def change_possibilities(amount,denominations)
   #sort array and remove duplicates
-  ordered = denominations.sort.uniq
+  count = 0
+  desc_ordered = denominations.sort.uniq.reverse
 
+  #just one denomination. no combos
+  desc_ordered.each { |denomination| count += 1 if amount % denomination == 0 }
+
+  #checking combos
+  # desc_ordered.each_with_index do |denomination, index|
+  #   leftover = amount - denomination
+  # end
+
+  count
 end
 
-puts "4 expected"
+puts "4 possibilities expected with 4 cents and 1,2,3cent coins"
 puts change_possibilities(4, [1,2,3])
